4afbf41de499178207581776d13cd039b6e9eedf
Omama Zainab <omi.zainy@gmail.com>
2020-09-30 16:09:40 +0500

    test

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import Store from './Context/Store'
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
+
   <Provider store={Store}>
-   
+    <BrowserRouter>
       <App />
-    
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
