ed11011e1eeefd181f8bd86ce7bf59442d4df21b
Omama Zainab <omi.zainy@gmail.com>
2020-09-30 16:10:54 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import {
-  BrowserRouter as Router,
+  // BrowserRouter as Router,
   Route,
   Routes
 } from "react-router-dom";
